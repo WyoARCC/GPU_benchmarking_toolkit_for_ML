7b51881ced1a9199086d87f7117f4e9f4be1414b
Tyson Limato <72205469+SlicedCubes@users.noreply.github.com>
2023-04-19 17:05:30 -0400

    Numerous Fixes
    + Properly loads Cuda and dependencies
    + Creates Transparent environment Variables for Pytorch to Grab
    + Properly Launches the main runtime in a Pytorch Distributed environment
    - Reduced CPU cores down to 10 from 16 because I was seeing roughly half utilization of 16 cores
    - Removed the improper seff jobid command

--- GPT2Scripting/BeartoothTrainGPT2.sh
+++ GPT2Scripting/BeartoothTrainGPT2.sh
@@ -5,20 +5,29 @@
 #SBATCH --job-name=GPUBenchMarkGPT2
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=10
 #SBATCH --mem=60G
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=tlimato@uwyo.edu
 #SBATCH --output=GPUBenchMarkGPT2_%A.log
-#SBATCH --gres=gpu:2
 #SBATCH --partition=beartooth-gpu
+#SBATCH --gres=gpu:2
+
 $SLURM_JOB_ID
 echo "Load Modules:"
 module load miniconda3/4.12.0
+module load sarcc/1.0
+module load gcc/11.2.0
+module load cuda/11.8.0
 echo "Activate Conda Environment"
-conda activate /pfs/tc1/project/arcc-students/tlimato/GPT2_SHDev/GPT2scripting
-python TrainGPT2WikiText.py
+conda activate /pfs/tc1/project/arcc-students/tlimato/GPT2_SHDev/GPT2Script
+echo "Creating Environment Variables"
+#Equal to Number of GPU's Requested
+export WORLD_SIZE=2
+#Automatically Handeled by SLURM
+export RANK=$SLURM_PROCID
+export LOCAL_RANK=$SLURM_LOCALID
+
+python torch.distributed.launch main.py
 
 conda deactivate
-
-seff jobid
