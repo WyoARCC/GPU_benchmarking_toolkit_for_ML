7fcf348128f3f24ee02a9e6ada91ed6c917dd857
Tyson Limato <72205469+SlicedCubes@users.noreply.github.com>
2023-04-19 16:59:08 -0400

    +Integrated the Pytorch Multi-GPU Requirements
    +from torch.nn.parallel import DistributedDataParallel
    +import torch.distributed as dist
    +Initializes a process Group from variables exported to the environment by the launcher batch file

--- GPT2Scripting/main.py
+++ GPT2Scripting/main.py
@@ -6,18 +6,28 @@ from transformers import GPT2LMHeadModel, GPT2Tokenizer
 from GPTData import GPTData
 from DataSetsForLLM import LoadWikiText
 import torch
-torch.cuda.empty_cache()
-
-from torch.optim import Adam
-from torch.utils.data import DataLoader
 from tqdm import tqdm
 import os
 import csv
+from torch.nn.parallel import DistributedDataParallel
+import torch.distributed as dist
+from torch.optim import Adam
+from torch.utils.data import DataLoader
+torch.cuda.empty_cache()
+
+# Initialize the process group
+dist.init_process_group(
+    backend="nccl",
+    init_method="tcp://%h:%x",
+    world_size=int(os.environ['WORLD_SIZE']),
+    rank=int(os.environ['RANK'])
+)
 
 
 # noinspection PyShadowingNames
 def train_and_validate(train_data, val_data, model_pass, optimizer):
     epochs = 10
+    model_pass = DistributedDataParallel(model_pass)  # integrate DataParallel
     for epoch in tqdm(range(epochs), desc='Epoch'):
         # Training loop
         for step, batch in tqdm(train_data, desc='Training Batch', leave=False):
@@ -57,8 +67,9 @@ def infer(inp):
 
 
 # Define Device for Training
-device = torch.device("cuda:0,1")  # if torch.cuda.is_available() else "cpu")
-
+device = torch.device("cuda")  # if torch.cuda.is_available() else "cpu")
+rank = dist.get_rank()
+torch.cuda.set_device(rank)
 # tokenizer Declaration and special token Declaration
 tokenizer = GPT2Tokenizer.from_pretrained("gpt2-xl")
 tokenizer.pad_token = '<pad>'
@@ -68,6 +79,11 @@ tokenizer.bos_token = '<bos>'
 # Model Declaration
 model = GPT2LMHeadModel.from_pretrained("gpt2-xl")
 model.resize_token_embeddings(len(tokenizer))
+
+# Wrap the model with DataParallel
+model = DistributedDataParallel(model, device_ids=[rank])
+
+# Move the model to the device
 model = model.to(device)
 
 # Load Wikitext-103-v1 Train Split and convert it to .json formatting
@@ -78,10 +94,19 @@ else:
     print('Generating Training JSON File...')
     train_data = LoadWikiText('train')
     train_data.save_to_json('wikitext-103-v1-train.json')
+
 # Instantiate preprocessing class object with current tokenizer and specified train dataset JSON file
 TrainChatData = GPTData('wikitext-103-v1-train.json', tokenizer)
-TrainChatData = DataLoader(TrainChatData, batch_size=8)
 
+# Create distributed version of the dataset
+print("Distributing Data Set...")
+train_sampler = torch.utils.data.distributed.DistributedSampler(
+    TrainChatData,
+    num_replicas=int(os.environ['WORLD_SIZE']),
+    rank=rank,
+    shuffle=True
+)
+TrainChatData = DataLoader(TrainChatData, batch_size=8, sampler=train_sampler)
 
 # Load Wikitext-103-v1 Validation Split and convert it to .json formatting
 if os.path.exists('wikitext-103-v1-validation.json'):
@@ -92,11 +117,19 @@ else:
     validation_data.save_to_json('wikitext-103-v1-validation.json')
 # Instantiate preprocessing class object with current tokenizer and specified train dataset JSON file
 ValidationChatData = GPTData('wikitext-103-v1-validation.json', tokenizer)
-ValidationChatData = DataLoader(ValidationChatData, batch_size=8)
 
+# Create distributed version of the dataset
+val_sampler = torch.utils.data.distributed.DistributedSampler(
+    ValidationChatData,
+    num_replicas=int(os.environ['WORLD_SIZE']),
+    rank=rank,
+    shuffle=False
+)
+ValidationChatData = DataLoader(ValidationChatData, batch_size=8, sampler=val_sampler)
 
 # Set Up Training
 model.train()
 optim = Adam(model.parameters(), lr=5e-6)
+
 # Call Training Function, Will write a CSV file
 train_and_validate(TrainChatData, ValidationChatData, model, optim)
