41f6f90a0a04217a97f071636ba9cfbb2b8f0702
Tyson Limato <72205469+SlicedCubes@users.noreply.github.com>
2023-05-06 15:15:41 -0600

    declare Initialization steps
    + necessary for gradient accumulation due to a version control mistake it wasn't declared on the GitHub repository

--- GPT2Scripting/main.py
+++ GPT2Scripting/main.py
@@ -25,6 +25,7 @@ def train_and_validate(trainer_data, val_data, model_pass, optimizer, scheduler_
     model_pass.train()
     train_loss = 0
     val_loss = 0
+    accumulation_steps = 16
     torch.set_default_tensor_type("torch.FloatTensor")
     # Training loop
     for batch in tqdm(trainer_data, desc='Training Batch', leave=False):
