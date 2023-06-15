d45b60f50de1b6ee1aee079c4c7bca88fdb78723
Tyson Limato <72205469+SlicedCubes@users.noreply.github.com>
2023-06-15 11:24:36 -0400

    Update DataSetsForLLM.py

--- GPT2Scripting/Non-JSON-Variant/DataSetsForLLM.py
+++ GPT2Scripting/Non-JSON-Variant/DataSetsForLLM.py
@@ -3,48 +3,58 @@
 # Model: Training GPT2 with Wikitext103-v1 from HuggingFace
 # Backend: Pytorch
 from datasets import load_dataset
-import json
-import os
 
 
 class WikiTextDataset:
-    def __init__(self, split, tokenizer):
+    def __init__(self, tokenizer, split):
+        # Load the WikiText dataset with the specified split
         self.data = load_dataset('wikitext', 'wikitext-103-v1', split=split)
         self.tokenizer = tokenizer
 
     def __getitem__(self, idx):
-        item = self.tokenizer(self.data[idx]['text'], truncation=True, max_length=128, padding=False,
+        # Tokenize the text at the given index using the tokenizer
+        item = self.tokenizer(self.data[idx]['text'], truncation=True, max_length=256, padding=False,
                               return_tensors='pt')
+        # Convert the tensors to long datatype and return them as a dictionary
         return {key: value.long() for key, value in item.items()}
 
     def __len__(self):
+        # Return the length of the dataset
+        return len(self.data)
+
+
+class BookCorpusDataset:
+    def __init__(self, tokenizer, split='train'):
+        # Load the BookCorpus dataset with the specified split
+        self.data = load_dataset('bookcorpus', split=split)
+        self.tokenizer = tokenizer
+
+    def __getitem__(self, idx):
+        # Tokenize the text at the given index using the tokenizer
+        item = self.tokenizer(self.data[idx]['text'], truncation=True, max_length=256, padding=False,
+                              return_tensors='pt')
+        # Convert the tensors to long datatype and return them as a dictionary
+        return {key: value.long() for key, value in item.items()}
+
+    def __len__(self):
+        # Return the length of the dataset
         return len(self.data)
 
 
 class OpenWebTextDataset:
     def __init__(self, tokenizer, split='train'):
+        # Load the OpenWebText dataset with the specified split
         self.data = load_dataset("openwebtext", split=split)
         self.tokenizer = tokenizer
 
     def __getitem__(self, idx):
-        item = self.tokenizer(self.data[idx]['text'], truncation=True, max_length=128, padding=False,
+        # Tokenize the text at the given index using the tokenizer
+        item = self.tokenizer(self.data[idx]['text'], truncation=True, max_length=256, padding=False,
                               return_tensors='pt')
+        # Convert the tensors to long datatype and return them as a dictionary
         return {key: value.long() for key, value in item.items()}
 
     def __len__(self):
+        # Return the length of the dataset
         return len(self.data)
 
-
-class LoadOscar:
-    def __init__(self, split):
-        self.split = split
-        self.data = load_dataset('oscar', 'unshuffled_deduplicated_en', split=self.split)
-        self.examples = [{'text': example} for example in self.data['text']]
-
-    @staticmethod
-    def create_directory(dir_path):
-        os.makedirs(dir_path, exist_ok=True)
-
-    def save_to_json(self, output_path):
-        with open(output_path, 'w', encoding='utf-8') as f:
-            json.dump({'data': self.examples}, f, ensure_ascii=False, indent=4)
